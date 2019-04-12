30ce794be35823b6964f9a2ecae8f075e68e6dee
Ankur Anant <ankuranant99@gmail.com>
2019-04-12 11:05:13 +0530

    initial commit

---
+++ Assets/gun.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class gun : NetworkBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 60f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    public GameObject impactEffect;
+    public ParticleSystem muzzleFlash;
+    //public ShootEffect shootEff;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire) {
+
+            nextTimeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+	}
+
+    [Command]
+    void CmdOnShoot()
+    {
+        RpcDoShootEffect();
+    }
+
+    [ClientRpc]
+    void RpcDoShootEffect()
+    {
+        Debug.Log("shoot not local");
+        muzzleFlash.Play();
+    }
+
+    [Client]
+    void Shoot() {
+
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.takeDamage(damage);
+            }
+
+            if(hit.rigidbody)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.2f);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Multiplayer FPS
+
+Multiplayer fps in unity using Unet(will be deprecated after 2019.4LTS)\
+currently abandoned
